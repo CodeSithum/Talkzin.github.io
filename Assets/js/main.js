99e6b4be24e21c650ea73d61004aaf594e8a96c3
sithum Udayanga <88572870+CodeSithum@users.noreply.github.com>
2023-11-21 12:16:32 +0530

    Update main.js

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,6 +1,6 @@
 window.addEventListener("unload", function (event) {
     $.ajax({
-        url: "/leaving-user-update/" + username + "",
+        url: "https://talkzin-9d83363b8c70.herokuapp.com/leaving-user-update/" + username + "",
         type: "PUT",
         success: function (response) {
             console.log(response);
@@ -8,7 +8,7 @@ window.addEventListener("unload", function (event) {
     });
 });
 $.ajax({
-    url: "/leaving-user-update/" + username + "",
+    url: "https://talkzin-9d83363b8c70.herokuapp.com/leaving-user-update/" + username + "",
     type: "PUT",
     success: function (response) {
         console.log(response);
